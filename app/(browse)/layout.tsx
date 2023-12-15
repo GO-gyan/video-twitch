913410839676b508d1b233396d8c523b815675ab
Gobinda Thakur <gobinda.thakur@outlook.com>
2023-12-15 20:03:32 +0530

    Added the navbar with logo, search input and user button

---
+++ app/(browse)/layout.tsx
@@ -0,0 +1,9 @@
+import Navbar from "./_components/navbar";
+export default function BrowseLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <Navbar />
+            <div className="flex h-full pt-20">{children}</div>
+        </>
+    );
+}
